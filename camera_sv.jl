9ec86c49fcad7099db145742d7a4de1233d54743
AntoAndGar <antonioandgargiulo@gmail.com>
2023-01-25 00:04:07 +0100

    finally a decent renderer with a good speed!

---
+++ camera_sv.jl
@@ -0,0 +1,60 @@
+include("raytrace_math.jl")
+
+struct Camera
+    lookfrom::Vec3 # origin
+    lookat::Vec3
+    vup::Vec3
+    vfov::Float64
+    aspect_ratio::Float64
+    aperture::Float64
+    focus_dist::Float64
+    origin::Vec3
+    lower_left_corner::Vec3
+    horizontal::Vec3
+    vertical::Vec3
+    u::Vec3
+    v::Vec3
+    w::Vec3
+    lens_radius::Float64
+
+    # Image
+    #aspect_ratio::Float64 = 16.0 / 9.0
+
+    # Camera
+    #focal_length::Float64 = 1.0
+    # theta = degrees_to_radians(vfov)
+    # h = tan(theta/2)
+    # viewport_height = 2.0 * h
+    # viewport_width = aspect_ratio * viewport_height
+
+    # w = unit_vector(lookfrom - lookat)
+    # u = unit_vector(cross(vup, w))
+    # v = cross(w, u)
+
+    Camera(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, focus_dist) = new(lookfrom,
+        lookat,
+        vup,
+        vfov, 
+        aspect_ratio,
+        aperture, 
+        focus_dist,
+        lookfrom, # origin
+        lookfrom - focus_dist * aspect_ratio * 2.0 * tan(deg2rad(vfov)/2.0) * unit_vector(cross(vup, unit_vector(lookfrom - lookat))) / 2.0 - focus_dist * 2.0 * tan(deg2rad(vfov)/2.0) * cross(unit_vector(lookfrom - lookat), unit_vector(cross(vup, unit_vector(lookfrom - lookat)))) / 2.0 - focus_dist * unit_vector(lookfrom - lookat),  # lower_left_corner
+        focus_dist * aspect_ratio * 2.0 * tan(deg2rad(vfov)/2.0) * unit_vector(cross(vup, unit_vector(lookfrom - lookat))),  # horizontal
+        focus_dist * 2.0 * tan(deg2rad(vfov)/2.0) * cross(unit_vector(lookfrom - lookat), unit_vector(cross(vup, unit_vector(lookfrom - lookat)))), # vertical
+        unit_vector(cross(vup, unit_vector(lookfrom - lookat))), # u
+        cross(unit_vector(lookfrom - lookat), unit_vector(cross(vup, unit_vector(lookfrom - lookat)))), # v
+        unit_vector(lookfrom - lookat), # w
+        aperture / 2.0 # lens_radius 
+        )
+end
+
+function get_ray(camera::Camera, s::Float64, t::Float64)
+    rd = camera.lens_radius * random_in_unit_disk()
+    offset = camera.u * rd[1] + camera.v * rd[2]
+
+    return Ray(
+        camera.origin + offset, 
+        camera.lower_left_corner + s * camera.horizontal + t * camera.vertical - camera.origin - offset
+        )
+end
---
+++ generate_scene_sv.jl
@@ -0,0 +1,127 @@
+include("vector_sv.jl")
+include("primitive_sv.jl")
+include("objects_structs_sv.jl")
+include("raytrace_math.jl")
+include("camera_sv.jl")
+using Images
+
+function write_color(im, pixel_color, samples_per_pixel)
+    scale = 1.0 / samples_per_pixel
+    r = clamp(sqrt(pixel_color[1] * scale), 0.0, 0.999)
+    g = clamp(sqrt(pixel_color[2] * scale), 0.0, 0.999)
+    b = clamp(sqrt(pixel_color[3] * scale), 0.0, 0.999)
+
+    return Vec3([r,g,b])
+    #write(im, "$r $g $b\n")
+end
+
+function save_image(filename::String, image_::Array{Vec3})
+    image = rotl90(image_)
+    
+    colored_image = zeros(RGB, size(image))
+    Threads.@threads for i in 1:size(image)[1]
+        Threads.@threads for j in 1:size(image)[2]
+            colored_image[i,j] = RGB(image[i,j][1], image[i,j][2], image[i,j][3])
+        end
+    end
+    save(filename, colored_image)
+  end
+
+function ray_color(ray::Ray, world::HittableList, depth::Int64)
+    hit_record = Hit_record(0. , Vec3([0.,0.,0.]), Vec3([0.,0.,0.]), Lambertian(), false)
+    
+    # If we've exceeded the ray bounce limit, no more light is gathered.
+    if depth <= 0
+        return Vec3([0.,0.,0.])
+    end
+
+    if hit(world, ray, 0.001 , Base.Inf64, hit_record)
+        scattered = Ray(Vec3([0.,0.,0.]), Vec3([0.,0.,0.]))
+        scat = scatter(hit_record.material, ray, hit_record, scattered)
+        if scat[1]
+            return scat[2] * ray_color(scattered, world, depth - 1)
+        end
+        return Vec3([0.,0.,0.])
+    end
+
+    unit_direction = unit_vector(ray.direction)
+    t = 0.5 * (unit_direction[2] + 1.0)
+    return (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
+end
+
+
+function random_scene()
+    world = hittable_list{Hittable}()
+    push!(world, Sphere(Vec3(0., -1000., 0.), 1000., Lambertian(Vec3(0.5, 0.5, 0.5))))
+    for a in -11:11
+        for b in -11:11
+            choose_mat = random_double()
+            center = Vec3(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double())
+            if norm(center - Vec3(4., 0.2, 0.)) > 0.9
+                if choose_mat < 0.8
+                    # diffuse
+                    albedo = Vec3(random_double() * random_double(), random_double() * random_double(), random_double() * random_double())
+                    push!(world, Sphere(center, 0.2, Lambertian(albedo)))
+                elseif choose_mat < 0.95
+                    # metal
+                    albedo = Vec3(random_double(0.5, 1.), random_double(0.5, 1.), random_double(0.5, 1.))
+                    fuzz = random_double(0., 0.5)
+                    push!(world,Sphere(center, 0.2, Metal(albedo, fuzz)))
+                else
+                    # glass
+                    push!(world, Sphere(center, 0.2, Dielectric(1.5)))
+                end
+            end
+        end
+    end
+
+    push!(world, Sphere(Vec3(0., 1., 0.), 1.0, Dielectric(1.5)))
+    push!(world, Sphere(Vec3(-4., 1., 0.), 1.0, Lambertian(Vec3(0.4, 0.2, 0.1))))
+    push!(world, Sphere(Vec3(4., 1., 0.), 1.0, Metal(Vec3(0.7, 0.6, 0.5), 0.0)))
+    return world
+end
+
+function main()
+    # Image
+    aspect_ratio = 3.0 / 2.0
+    image_w = 720
+    image_h =  trunc(Int64, image_w / aspect_ratio)
+    samples_per_pixel = 500
+    max_depth = 50
+
+    # World
+    world = random_scene()
+
+    # Camera
+    lookfrom = Vec3(13., 2., 3.)
+    lookat = Vec3(0., 0., 0.)
+    vup = Vec3(0., 1., 0.)
+    aperture = 0.1
+    dist_to_focus = 10.0
+
+    camera = Camera(lookfrom, lookat, vup, 20.0, aspect_ratio, aperture, dist_to_focus)
+
+    image = zeros(Vec3, image_w, image_h)
+
+    #write(im, "P3\n$image_w $image_h\n255\n")
+
+    Threads.@threads for i in 1:image_w
+        println("Doing column: $i")
+        Threads.@threads for j in 1:image_h
+            image[i,j] = Vec3(zeros(Float64, 1, 3))
+            for s in 1:samples_per_pixel
+                u = (i + random_double()) / (image_w - 1)
+                v = (j + random_double()) / (image_h - 1)
+                r = get_ray(camera, u, v)
+                image[i,j] += ray_color(r, world, max_depth)
+            end
+            image[i,j] = write_color(im, image[i,j], samples_per_pixel)
+        end
+        #save_image("raytracing_vec1.png", image)
+    end
+
+    save_image("raytracing_vec_sv.png", image)
+    println("Done!")
+end
+
+main()
